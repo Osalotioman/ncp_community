995663824be8bb590b2d46a3dc8000a5d76e4aef
Osazuwa Emmanuel Osalotioman <109004799+OsazuwaEmmanuel@users.noreply.github.com>
2024-04-26 09:01:31 +0100

    Update links.js

--- links.js
+++ links.js
@@ -1,9 +1,6 @@
-// Create a div element to contain the new content
 var div = document.createElement('div');
-// Create an h2 element
 var h2 = document.createElement('h2');
-h2.textContent = 'Repository Pages'; // Set the text content of the h2 element
-// Create a ul element
+h2.textContent = 'Repository Pages';
 var ul = document.createElement('ul');
 var links = [
     "https://docs.google.com/spreadsheets/d/1EMK2fXn6rR_q4wJV4fU-CR55GBwVqwWYESp7DDpYD4U/edit?usp=sharing",
@@ -23,35 +20,7 @@ for(let i=0; i<links.length; ++i){
     li1.appendChild(a1);
     ul.appendChild(li1);
 }
-// Append h2 and ul to the div
 div.appendChild(h2);
 div.appendChild(ul);
-/*
-// Create li elements with anchor (a) elements inside
-var li1 = document.createElement('li');
-var a1 = document.createElement('a');
-a1.href = '';
-a1.textContent = 'Updating Upsolves';
-li1.appendChild(a1);
-
-var li2 = document.createElement('li');
-var a2 = document.createElement('a');
-a2.href = '';
-a2.textContent = '';
-li2.appendChild(a2);
-
-var li3 = document.createElement('li');
-var a3 = document.createElement('a');
-a3.href = '';
-a3.textContent = '';
-li3.appendChild(a3);
-
-// Append li elements to the ul
-ul.appendChild(li1);
-ul.appendChild(li2);
-ul.appendChild(li3);
-*/
-
-// Get the div with ID 'content' and append the created div to it
 var contentDiv = document.getElementById('links');
 contentDiv.appendChild(div);
