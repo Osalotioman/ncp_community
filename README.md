224dbe50826688a719daaecda7ebc20890ab1a0b
Osazuwa Emmanuel Osalotioman <109004799+OsazuwaEmmanuel@users.noreply.github.com>
2024-07-20 04:31:36 +0000

    New Page Added - Fetch Users from an organization on codeforces.

--- README.md
+++ README.md
@@ -1,4 +1,7 @@
 ## What's new?
+
+[Fetch Users from an organization on codeforces.](https://osalotioman.github.io/ncp_community/cf_users_from_an_org.html)
+
 [Updating Upsolves](https://docs.google.com/spreadsheets/d/1EMK2fXn6rR_q4wJV4fU-CR55GBwVqwWYESp7DDpYD4U/edit?usp=sharing)
 
 [Get Unsolved and Solved Problems of Codeforces User](https://osalotioman.github.io/ncp_community/get_unsolved_and_solved_problems.html)
--- four_pupils/script.js
+++ four_pupils/script.js
@@ -7,71 +7,67 @@ const users = [
     { name: 'Cyberdude', handle: 'cyberdude', rating: 742 },
     { name: 'Otutochi', handle: 'Otuz', rating: 620},
     { name: 'Cryptickid91', handle: 'cryptickid91', rating: 884 }
-    // Hey there, you can add your data here
 ];
+
 const pts = [
     {name: '<b style="color:#008000;">TechWiz</b>', handle: '0x000022', rating: 1187},
     {name: '<b style="color:#03A89E;">Eti-ini</b>', handle: 'randombernie', rating: 1375},
-    {name: '<b style="color:#008000;">Dreadedhippy</b>', handle: 'dreadedhippy', rating:1310},
+    {name: '<b style="color:#008000;">Dreadedhippy</b>', handle: 'dreadedhippy', rating: 1310},
     {name: '<b style="color:#03A89E;">Ehijoe</b>', handle: 'ehijoe', rating: 1345}
-]
-async function populateTable() {
-const tableBody = document.getElementById('ratingTableBody');
-const sortedUsers = users.sort((a, b) => b.rating - a.rating);
-let uid = 1;
-sortedUsers.forEach(user => {
-    const adjustedRating = 1200 - user.rating; // Calculate adjusted rating
-    const profileLink = `https://codeforces.com/profile/${user.handle}`; // Codeforces profile link
-    const row = `<tr><td>${uid}</td><td>${user.name}</td><td>${user.rating}</td><td>${adjustedRating}</td><td><a href="${profileLink}" target="_blank">Profile</a></td></tr>`;
-    tableBody.insertAdjacentHTML('beforeend', row);
-    ++uid;
-});
-const tableBody_pts = document.getElementById('ptsb');
-const sortedUsers_pts = pts.sort((a, b) => b.rating - a.rating);
-uid = 1;
-sortedUsers_pts.forEach(pts => {
-    const adjustedRating_pts = 1400 - pts.rating; // Calculate adjusted rating
-    const profileLink_pts = `https://codeforces.com/profile/${pts.handle}`; // Codeforces profile link
-    const row_pts = `<tr><td>${uid}</td><td>${pts.name}</td><td>${pts.rating}</td><td>${adjustedRating_pts}</td><td><a href="${profileLink_pts}" target="_blank">Profile</a></td></tr>`;
-    tableBody_pts.insertAdjacentHTML('beforeend', row_pts);
-    ++uid;
-});
-}
-window.onload = populateTable;
+];
 
-const users_archive = [
-    [
-        { name: 'Emmanuel', handle: 'OsazuwaEmmanuel', rating: 629 },
-        { name: 'Genesis', handle: 'daveshub', rating: 783 },
-        { name: 'Minerva', handle: 'Minerva2005', rating: 932 },
-        { name: 'Turing_apple', handle: 'Turing_apple', rating: 847 },
-        { name: 'python_tamer', handle: 'python_tamer', rating: 1107 },
-        { name: 'cyberdude', handle: 'cyberdude', rating: 742 },
-        { name: 'cryptickid91', handle: 'cryptickid91', rating: 566 },
-        {name: 'randombernie', handle: 'randombernie', rating: 1160},
-        {name: '0x000022', handle: '0x000022', rating: 1107}
-        // Hey there, you can add your data here
-    ],
-    [//30th April 2024
-        { name: 'Emmanuel', handle: 'OsazuwaEmmanuel', rating: 541 },
-        { name: 'Genesis', handle: 'daveshub', rating: 783 },
-        { name: 'Minerva', handle: 'Minerva2005', rating: 932 },
-        { name: 'Turing_apple', handle: 'Turing_apple', rating: 847 },
-        { name: 'python_tamer', handle: 'python_tamer', rating: 1107 },
-        { name: 'cyberdude', handle: 'cyberdude', rating: 742 },
-        { name: 'cryptickid91', handle: 'cryptickid91', rating: 566 },
-        {name: 'randombernie', handle: 'randombernie', rating: 1149},
-        {name: 'dreadedhippy', handle: 'dreadedhippy', rating:1161},
-        {name: '0x000022', handle: '0x000022', rating: 1107}
-        // Hey there, you can add your data here
-    ]
-]
-const pts_archive = [
-    [
-        {name: 'dreadedhippy', handle: 'dreadedhippy', rating:1256},
-        {name: 'ehijoe', handle: 'ehijoe', rating: 1305}
-    ],
-    [//30th April 2024
-        {name: 'ehijoe', handle: 'ehijoe', rating: 1332}
-    ]
-]
+async function fetchRatings(handles) {
+    const url = `https://codeforces.com/api/user.info?handles=${handles.join(';')}`;
+    const response = await fetch(url);
+    const data = await response.json();
+    return data.result;
+}
+
+async function updateUserRatings() {
+    const userHandles = users.map(user => user.handle);
+    const ptsHandles = pts.map(pt => pt.handle);
+
+    const userRatings = await fetchRatings(userHandles);
+    const ptsRatings = await fetchRatings(ptsHandles);
+
+    userRatings.forEach(ratingInfo => {
+        const user = users.find(u => u.handle === ratingInfo.handle);
+        if (user) user.rating = ratingInfo.rating;
+    });
+
+    ptsRatings.forEach(ratingInfo => {
+        const pt = pts.find(p => p.handle === ratingInfo.handle);
+        if (pt) pt.rating = ratingInfo.rating;
+    });
+}
+
+function populateTable() {
+    const tableBody = document.getElementById('ratingTableBody');
+    const sortedUsers = users.sort((a, b) => b.rating - a.rating);
+    let uid = 1;
+    sortedUsers.forEach(user => {
+        const adjustedRating = 1200 - user.rating;
+        const profileLink = `https://codeforces.com/profile/${user.handle}`;
+        const row = `<tr><td>${uid}</td><td>${user.name}</td><td>${user.rating}</td><td>${adjustedRating}</td><td><a href="${profileLink}" target="_blank">Profile</a></td></tr>`;
+        tableBody.insertAdjacentHTML('beforeend', row);
+        ++uid;
+    });
+
+    const tableBody_pts = document.getElementById('ptsb');
+    const sortedUsers_pts = pts.sort((a, b) => b.rating - a.rating);
+    uid = 1;
+    sortedUsers_pts.forEach(pt => {
+        const adjustedRating_pts = 1400 - pt.rating;
+        const profileLink_pts = `https://codeforces.com/profile/${pt.handle}`;
+        const row_pts = `<tr><td>${uid}</td><td>${pt.name}</td><td>${pt.rating}</td><td>${adjustedRating_pts}</td><td><a href="${profileLink_pts}" target="_blank">Profile</a></td></tr>`;
+        tableBody_pts.insertAdjacentHTML('beforeend', row_pts);
+        ++uid;
+    });
+}
+
+async function initialize() {
+    await updateUserRatings();
+    populateTable();
+}
+
+window.onload = initialize;
